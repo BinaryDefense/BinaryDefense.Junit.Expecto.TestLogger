e502a2a0e153b1eac3b2e7b0762af0aec083eb50
Max Paige <max.paige@binarydefense.com>
2021-01-29 14:07:21 -0500

    Bump version to 0.2.0
    ## [0.2.0] - 2021-01-29
    
    ### Added
    * Allow quotes to prevent splitting on characters in test names for reporting.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,11 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.2.0] - 2021-01-29
+
+### Added
+* Allow quotes to prevent splitting on characters in test names for reporting.
+
 ## [0.1.0] - 2021-01-08
 
 ### Added
@@ -12,5 +17,6 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 * Specify name format for lists in test names
 * Specify delimiter for lists in test names
 * Specify file path for report output
-[Unreleased]: https://github.com/BinaryDefense/BinaryDefense.Junit.Expecto.TestLogger/compare/v0.1.0...HEAD
+[Unreleased]: https://github.com/BinaryDefense/BinaryDefense.Junit.Expecto.TestLogger/compare/v0.2.0...HEAD
+[0.2.0]: https://github.com/BinaryDefense/BinaryDefense.Junit.Expecto.TestLogger/compare/v0.1.0...v0.2.0
 [0.1.0]: https://github.com/BinaryDefense/BinaryDefense.Junit.Expecto.TestLogger/releases/tag/v0.1.0
--- src/BinaryDefense.Junit.Expecto.TestLogger/AssemblyInfo.fs
+++ src/BinaryDefense.Junit.Expecto.TestLogger/AssemblyInfo.fs
@@ -4,20 +4,20 @@ open System.Reflection
 
 [<assembly: AssemblyTitleAttribute("BinaryDefense.Junit.Expecto.TestLogger")>]
 [<assembly: AssemblyProductAttribute("BinaryDefense.Junit.Expecto.TestLogger")>]
-[<assembly: AssemblyVersionAttribute("0.1.0")>]
-[<assembly: AssemblyMetadataAttribute("ReleaseDate","2021-01-08T00:00:00.0000000-05:00")>]
-[<assembly: AssemblyFileVersionAttribute("0.1.0")>]
-[<assembly: AssemblyInformationalVersionAttribute("0.1.0")>]
+[<assembly: AssemblyVersionAttribute("0.2.0")>]
+[<assembly: AssemblyMetadataAttribute("ReleaseDate","2021-01-29T00:00:00.0000000-05:00")>]
+[<assembly: AssemblyFileVersionAttribute("0.2.0")>]
+[<assembly: AssemblyInformationalVersionAttribute("0.2.0")>]
 [<assembly: AssemblyMetadataAttribute("ReleaseChannel","release")>]
-[<assembly: AssemblyMetadataAttribute("GitHash","04167b9df94781bdb011b5a553782ad5f775a90a")>]
+[<assembly: AssemblyMetadataAttribute("GitHash","7a28a03ecf7ca4351b529f9843577816d0ffc82e")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "BinaryDefense.Junit.Expecto.TestLogger"
     let [<Literal>] AssemblyProduct = "BinaryDefense.Junit.Expecto.TestLogger"
-    let [<Literal>] AssemblyVersion = "0.1.0"
-    let [<Literal>] AssemblyMetadata_ReleaseDate = "2021-01-08T00:00:00.0000000-05:00"
-    let [<Literal>] AssemblyFileVersion = "0.1.0"
-    let [<Literal>] AssemblyInformationalVersion = "0.1.0"
+    let [<Literal>] AssemblyVersion = "0.2.0"
+    let [<Literal>] AssemblyMetadata_ReleaseDate = "2021-01-29T00:00:00.0000000-05:00"
+    let [<Literal>] AssemblyFileVersion = "0.2.0"
+    let [<Literal>] AssemblyInformationalVersion = "0.2.0"
     let [<Literal>] AssemblyMetadata_ReleaseChannel = "release"
-    let [<Literal>] AssemblyMetadata_GitHash = "04167b9df94781bdb011b5a553782ad5f775a90a"
+    let [<Literal>] AssemblyMetadata_GitHash = "7a28a03ecf7ca4351b529f9843577816d0ffc82e"
